22de0336809c9595601e9bdac4ee8c2cb36e6343
Pranith Mahanti <pranithmahanti@gmail.com>
2025-08-31 13:50:28 +0530

    Preprocessing comments added

--- preprocess.py
+++ preprocess.py
@@ -14,14 +14,34 @@ from nltk.corpus import wordnet
 from nltk.stem import WordNetLemmatizer
 
 import contractions
+
 import pandas as pd
 
-# Remove all numericals, special characters and punctuation marks
+
 def clean(text):
+    '''
+    Remove all numericals, special characters and punctuation marks
+
+    Args:
+        text (str): Regular unprocessed news headlines
+    Returns:
+        str: Processed and clean text
+
+    '''
     text = re.sub('[^A-Za-z]+', ' ', text) 
     return text
 
 def expand_contractions(text):
+    '''
+    Replace all the contractions with their expanded equivalents
+
+    Args:
+        text (str): Processed data 
+                    (preferrably output from clean func.)
+    Returns:
+        str: Text with expanded equivalents of contractions.
+        
+    '''
     tkn = text.split(" ")
     expanded = []
 
@@ -31,7 +51,18 @@ def expand_contractions(text):
     return " ".join(expanded)
 
 def POS_tags(text):
-    # Remove Stop words
+    '''
+    Remove the stopwords and tokenize the text along with POS(Parts of speech) tags.
+
+    Args:
+        text (str): Processed data with expanded contractions 
+                    (preferrably output from expand_contractions func.)
+    Returns:
+        list: A list of tuples that occur in pairs of words abd the respective pos.
+              Note, the only pos that will be reported are [Adjectives, Verbs, Nouns, Adverbs] 
+              since they are the only ones to be lemmatized.
+        
+    '''
     stop_words = set(stopwords.words('english'))
     tokens = word_tokenize(text.lower())
 
@@ -46,6 +77,16 @@ def POS_tags(text):
     return pos_tagged
 
 def lemmatize(pos_data):
+    '''
+    Replace all the contractions with their expanded equivalents
+
+    Args:
+        pos_data (list): A list of tuples of form (word, pos)
+                         (preferrably output of POS_tags func.)
+    Returns:
+        str: A string with only lemmatized words. Can be seperated easily using split() function.
+        
+    '''
     lemma_final = ""
 
     wordnet_lemmatizer = WordNetLemmatizer()
@@ -60,16 +101,33 @@ def lemmatize(pos_data):
 
     return lemma_final.strip()
 
-def preprocess(data):
-    text = data['text']
-    data["Cleaned"] = text.apply(expand_contractions).apply(clean)
-    data["POS tagged"] = data["Cleaned"].apply(POS_tags)
-    data["Lemmatized"] = data["POS tagged"].apply(lemmatize)
+def preprocess(series):
+    '''
+    Replace all the contractions with their expanded equivalents
 
-    return data
+    Args:
+        series (pd.Series): 
+            A pandas Series which should contain a column titled <text>
+    Returns:
+        pd.Series: 
+            A pandas Series with added columns 
+                <Cleaned>   : Cleaned data with expanded contractions and with numericals, special char and punctuations removed.
+                <POS tagged>: List of tuples of form (word, pos)
+                <Lemmatized>: Strings of lemmatized text (Final Output)
+        
+    '''
+    text = series['text']
+    series["Cleaned"] = text.apply(expand_contractions).apply(clean)
+    series["POS tagged"] = series["Cleaned"].apply(POS_tags)
+    series["Lemmatized"] = series["POS tagged"].apply(lemmatize)
+
+    return series
 
 
+# For the purpose of testing
+# test_phrases.csv: Has all sample headlines.
+# final_output.csv: Well, the name explains it, pretty much.
 if __name__ == "__main__":
     data = pd.read_csv('test_phrases.csv')
-    print(preprocess(data))
+    preprocess(data).to_csv('final_output.txt')
 
